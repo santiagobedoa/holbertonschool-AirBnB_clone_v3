a64aec13277640f5db0dd37584483d887315be1c
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-16 16:06:11 -0500

    problem with foreing keys solved

--- models/place.py
+++ models/place.py
@@ -24,7 +24,7 @@ class Place(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'places'
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=True)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
