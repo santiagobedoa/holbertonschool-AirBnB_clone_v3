75bdad8ee547a6466ed26f3c2ac6d2641a3c124e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-16 17:01:13 -0500

    3. Improve storage - task added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -15,12 +21,12 @@ def status():
 def stats():
     """Returns the number of each objects by type"""
     objects = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
     }
     objects = {k: storage.count(v) for k, v in objects.items()}
     return jsonify(objects)
