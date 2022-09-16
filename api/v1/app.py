c8116d7b273e311954ceb0be39505b5b863f4951
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-16 17:21:59 -0500

    6. Not found - task added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """Module for Flask REST app"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 not found message"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
